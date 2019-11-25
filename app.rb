b0b04c8b5d99fbcefbf3185161be0c355e78f5b1
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-25 16:16:04 +0000

    viewing bookmarks feature test

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
 
-  get '/' do
-    'Hello World'
-  end
+  get '/bookmarks' do
+  bookmarks = [
+            "http://www.makersacademy.com",
+            "https://www.tutorialspoint.com"
+           ]
+
+  bookmarks.join
+end
 
   run! if app_file == $0
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,8 @@
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
+  scenario 'a user can see bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "https://www.tutorialspoint.com"
   end
 end
