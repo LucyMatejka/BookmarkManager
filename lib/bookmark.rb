4c0edc80ecd4fd8347f9f5e6fad10169f7409d3a
Lucy Matejka <lucymatejka@yahoo.co.uk>
2020-01-13 17:10:59 +0000

    final commit

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -26,13 +26,16 @@
    end
 
    def self.create(url:, title:)
+     return false unless is_url?(url)
      if ENV['ENVIRONMENT'] == 'test'
        connection = PG.connect(dbname: 'bookmark_manager_test')
      else
        connection = PG.connect(dbname: 'bookmark_manager')
      end
 
-     connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}','#{url}') RETURNING id, url, title ")
+     result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}','#{url}') RETURNING id, url, title ")
+     Bookmark.new(id: result[0]['id'], title: result[0]['title'], utl: result[0]['url'])
      #SQL commands
+     #check connection.exec line
    end
  end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -25,7 +25,8 @@ end
 describe '.create' do
   it 'creates a new bookmark' do
     bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
-    persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+    persisted_data = persisted_data(id: bookmark.id)
+    #persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
 
     expect(bookmark).to be_a Bookmark
     expect(bookmark.id).to eq persisted_data.first['id']
